10c5954363add576e464eebfb7a02f5a55124c2b
Russell Gordon <rgordon@lcs.on.ca>
2023-11-08 15:54:48 -0500

    Initial Commit

---
+++ StacksExample/StacksExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksExampleApp.swift
+//  StacksExample
+//
+//  Created by Russell Gordon on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct StacksExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
